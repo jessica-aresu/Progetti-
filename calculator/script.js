18bd25c176f10bfa2b7e730df4732a62e8d4eabe
Jessica Aresu <87132037+jessica-aresu@users.noreply.github.com>
2021-09-15 10:39:10 +0200

    Add files via upload
    Calcolatrice semplice

---
+++ calculator/script.js
@@ -0,0 +1,14 @@
+function insert(num){
+    document.form.textview.value=document.form.textview.value+num
+}
+function equal(){
+    document.form.textview.value=eval(document.form.textview.value)
+}
+function clean(){
+    document.form.textview.value=''
+
+}
+function back(){
+    document.form.textview.value=''
+}
+
